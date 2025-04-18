157745fd82346c4775bd8f836af9b5ff5abd18e6
Chris Pendleton <128199405+Electrostud@users.noreply.github.com>
2025-04-18 14:30:01 -0400

    Delete Auto_Smart_EV_Irrigation_Scheduler/tests directory

--- Auto_Smart_EV_Irrigation_Scheduler/tests/test_auto_smart_ev_irrigation_scheduler.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from homeassistant.setup import async_setup_component
-from custom_components.auto_smart_ev_irrigation_scheduler import DOMAIN
-
-@pytest.mark.asyncio
-async def test_setup(hass):
-    """Test the integration is set up correctly."""
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {}})
-    assert DOMAIN in hass.data
